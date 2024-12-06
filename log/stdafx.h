c0745285b008488755c4dffd2404cb66d113fba3
tencenthost <1780081362@qq.com>
2024-12-06 14:35:17 +0800

    upload log file

---
+++ log/stdafx.h
@@ -0,0 +1,19 @@
+#ifndef __STDAFX_H__
+#define __STDAFX_H__
+
+/**
+ * @desrcibe:通用头文件保存，后续使用的文件可以直接包含此文件即可
+ */
+
+
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <string.h>
+#endif
